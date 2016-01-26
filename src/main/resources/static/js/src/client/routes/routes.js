8b6a26ce96987cf6158951a7294957130592cd76
psychobolt <psychobolt@gmail.com>
2016-01-25 20:56:01 -0800

    update README.md with front-end devel guide and add template Week route

--- src/main/resources/static/js/src/client/routes/routes.js
+++ src/main/resources/static/js/src/client/routes/routes.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 import { createHistory } from 'history';
 import App from './App.js';
+import Week from './Week.js';
 
 export default function() {
     const history = createHistory();
@@ -12,6 +13,7 @@ export default function() {
             <Route path="/" component="div">
                 <IndexRoute component={ App } />
                 <Route path="/" component={ App } />
+                <Route path="/week/:id" component={ Week } />
             </Route>
         </Router>
         );
