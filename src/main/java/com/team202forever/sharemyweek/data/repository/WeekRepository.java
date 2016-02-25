545cf1da6a00f510f5e59744cf8d76b95d358c1b
psychobolt <psychobolt@gmail.com>
2016-02-24 22:56:29 -0800

    Push missing commit

--- src/main/java/com/team202forever/sharemyweek/data/repository/WeekRepository.java
+++ src/main/java/com/team202forever/sharemyweek/data/repository/WeekRepository.java
@@ -5,6 +5,12 @@ import com.team202forever.sharemyweek.data.models.HashId;
 import com.team202forever.sharemyweek.data.models.Week;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 @SuppressWarnings("unused")
 public interface WeekRepository extends MongoRepository<Week, HashId> {
+
+    List<Week> findAllByOrderByCreationDateAsc();
+
+    List<Week> findAllByOrderByCreationDateDesc();
 }
