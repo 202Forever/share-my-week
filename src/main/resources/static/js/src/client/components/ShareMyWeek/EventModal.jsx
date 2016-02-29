81a01d7be76544e94048e2bd30d3bd664ad1680d
psychobolt <psychobolt@gmail.com>
2016-02-28 16:23:06 -0800

    Fit Event modal input fields on small devices

--- src/main/resources/static/js/src/client/components/ShareMyWeek/EventModal.jsx
+++ src/main/resources/static/js/src/client/components/ShareMyWeek/EventModal.jsx
@@ -14,24 +14,24 @@ class EventModal extends Component {
                 <Modal.Body>
                     <Grid fluid={true}>
                         <row>
-                            <Col md={6}>
+                            <Col md={6} sm={6} xs={6}>
                                 <FormControls.Static label="When" value={<FormattedDate value={start} day="numeric" month="long" year="numeric" />} />
                             </Col>
-                            <Col md={3}>
+                            <Col md={3} sm={3} xs={6}>
                                 <TimeSelect label="Start time" value={start.toDate()} start={0} locale="en-US"/>
                             </Col>
-                            <Col md={3}>
+                            <Col md={3} sm={3} xs={6}>
                                 <TimeSelect label="End time" value={end.toDate()} end={2359} locale="en-US" />
                             </Col>
                         </row>
                         <row>
-                            <Col md={6} mdPull={6}>
+                            <Col md={6} mdPull={6} sm={6} smPull={6} xs={9}>
                                 <Input type="text" label="What" />
                             </Col>
-                            <Col md={6}><span/></Col>
+                            <Col md={6} sm={6} xs={3}><span/></Col>
                         </row>
                         <row>
-                            <Col md={12}>
+                            <Col md={12} sm={12} xs={12}>
                                 <Input type="textarea" label="Description" rows="3" />
                             </Col>
                         </row>
