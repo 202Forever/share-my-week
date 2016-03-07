075105ed22bf75cf71714b3219eefc253f33e008
psychobolt <psychobolt@gmail.com>
2016-03-07 13:51:38 -0800

    fix copy clipboard on safari

--- src/main/resources/static/js/src/client/components/ShareMyWeek/ShareComponents.jsx
+++ src/main/resources/static/js/src/client/components/ShareMyWeek/ShareComponents.jsx
@@ -1,20 +1,54 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Grid, Row, Col, Panel, Input, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Panel, Input, Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import ClipboardButton from 'react-clipboard.js';
 import { saveWeek } from '../../actions/serverActions';
 
 const seperatorsRegEx = new RegExp([' ', ','].join('|'), "g");
+
+// Simplistic detection, do not use it in production
+function fallbackMessage(action) {
+    var actionMsg = '';
+    var actionKey = (action === 'cut' ? 'X' : 'C');
+
+    if(/iPhone|iPad/i.test(navigator.userAgent)) {
+        actionMsg = 'No support :(';
+    }
+    else if (/Mac/i.test(navigator.userAgent)) {
+        actionMsg = 'Press ⌘-' + actionKey + ' to ' + action;
+    }
+    else {
+        actionMsg = 'Press Ctrl-' + actionKey + ' to ' + action;
+    }
+
+    return actionMsg;
+}
 
 class ShareComponents extends Component {
 
     constructor(props, content) {
         super(props, content);
         this.state = {
-            emails: ''
+            emails: '',
+            copyTooltip: props.copyTooltip
         };
+        this.onCopySuccess = this.onCopySuccess.bind(this);
+        this.onCopyError = this.onCopyError.bind(this);
         this.onEmailsChange = this.onEmailsChange.bind(this);
         this.onInvite = this.onInvite.bind(this);
+        this.resetCopyTooltip = this.resetCopyTooltip.bind(this);
+    }
+
+    onCopySuccess(e) {
+        const {copyTooltipTrigger} = this.refs;
+        this.setState({copyTooltip: 'Copied!'});
+        copyTooltipTrigger.show();
+    }
+
+    onCopyError(e) {
+        const {copyTooltipTrigger} = this.refs;
+        this.setState({copyTooltip: fallbackMessage(e.action)});
+        copyTooltipTrigger.show();
     }
 
     onEmailsChange(e) {
@@ -34,8 +68,13 @@ class ShareComponents extends Component {
         dispatch(action);
     }
 
+    resetCopyTooltip() {
+        this.setState({copyTooltip: ShareComponents.defaultProps.copyTooltip});
+    }
+
     render() {
         const {pathname} = this.props;
+        const tooltip = (<Tooltip ref="copyBtnTooltip" id="copyBtnTooltip">{this.state.copyTooltip}</Tooltip>);
         return (<Grid
                       {...this.props}
                       fluid={ true }>
@@ -46,12 +85,23 @@ class ShareComponents extends Component {
                              md={ 3 }
                              sm={ 4 }>
                         <Panel>
-                            <Input
+                            <Input id="public_url"
                                    type="text"
                                    label="Public Week URL"
-                                   disabled={ true }
                                    style={{cursor:"text"}}
-                                   buttonAfter={<ClipboardButton className="btn btn-default" data-clipboard-text={location.origin + pathname}><i className="fa fa-clipboard" /></ClipboardButton>}
+                                   buttonAfter={
+                                        <OverlayTrigger delay={1000}
+                                            ref="copyTooltipTrigger" onExited={this.resetCopyTooltip}
+                                            placement="top" overlay={tooltip} trigger="click">
+                                            <ClipboardButton className="btn btn-default"
+                                                onSuccess={this.onCopySuccess}
+                                                onError={this.onCopyError}
+                                                data-clipboard-target="#public_url">
+                                                <i className="fa fa-clipboard" />
+                                            </ClipboardButton>
+                                        </OverlayTrigger>
+                                    }
+                                    readOnly={true}
                                    value={location.origin + pathname} />
                         </Panel>
                         </Col>
@@ -75,6 +125,10 @@ class ShareComponents extends Component {
             );
     }
 }
+
+ShareComponents.defaultProps = {
+    copyTooltip: 'Copy'
+};
 
 function mapStateToProps(state = {}, props) {
     const {routing, appData} = state;
